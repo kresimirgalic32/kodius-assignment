fc22b823374b2b67d6e6ad333b6ac4a672f933a8
kresimirgalic32 <kresimir.galic32@gmail.com>
2022-03-08 18:22:38 +0100

    data and server update, added userRouter and userModel

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,20 @@
+import bcrypt from "bcryptjs";
 const data = {
+  users: [
+    {
+      name: "Kreso",
+      email: "kresimir.galic32@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: true,
+    },
+    {
+      name: "idixon",
+      email: "idixon323@gmail.com",
+      password: bcrypt.hashSync("1234", 8),
+      isAdmin: false,
+    },
+  ],
+
   products: [
     {
       id: "1",
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
+import userRouter from "./routers/userRouter.js";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/kodius");
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
 });
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Server is ready");
 });
+// eslint-disable-next-line no-undef
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server at http://localhost:${port}`);
