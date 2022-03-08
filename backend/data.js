8535d81b845eb17179b8c7e489fdfd6d8318ef2f
kresimirgalic32 <kresimir.galic32@gmail.com>
2022-03-08 12:11:10 +0100

    added node server, moved data and implemented nodemon

---
+++ backend/data.js
@@ -0,0 +1,39 @@
+const data = {
+  products: [
+    {
+      id: "1",
+      name: "Smart Hub",
+      price: 49.99,
+      image:
+        "https://shop.bt.com/images/product/uni2/DigitalContent/dy/DYNK_A24D1E3D-BB50-490D-9E5F-DFB5B8A63C9B_large.jpg",
+    },
+    {
+      id: "2",
+      name: "Motion Sensor",
+      price: 24.99,
+      image:
+        "https://mobileimages.lowes.com/productimages/66029aab-53ba-4e5b-87c3-f62bd5a9a15d/15276091.jpg?size=pdhi",
+    },
+    {
+      id: "3",
+      name: "Wireless Camera",
+      price: 99.99,
+      image: "https://m.media-amazon.com/images/I/31taIOBAhpL.jpg",
+    },
+    {
+      id: "4",
+      name: "Smoke Sensor",
+      price: 19.99,
+      image:
+        "https://www.legrand.com/ecatalogue/media/catalog/product/cache/4/image/650x650/9df78eab33525d08d6e5fb8d27136e95/l/g/LG-643072-WEB-F.jpg",
+    },
+    {
+      id: "5",
+      name: "Water Leak Sensor",
+      price: 14.99,
+      image:
+        "https://cdn.antratek.nl/media/product/72c/lwl02-lorawan-water-leak-sensor-lwl02-eu868-a6c.jpg",
+    },
+  ],
+};
+export default data;
