5c8bece82817f4a1defc0ad0055f071e5a268724
kresimirgalic32 <kresimir.galic32@gmail.com>
2022-03-14 19:17:46 +0100

    ttt

--- frontend/src/Screens/Home.js
+++ frontend/src/Screens/Home.js
@@ -9,7 +9,7 @@ const Home = (props) => {
 
   const { products } = data;
 
-  const [cartItems, setCartItems] = useState([]);
+  const [cartItems, setCartItems] = useState(cartItemsLoad);
   useEffect(() => {
     localStorage.setItem("cartItems", JSON.stringify(cartItems));
   }, [cartItems]);
@@ -19,30 +19,30 @@ const Home = (props) => {
     // saveCartItems(product);
     // const varijabla = JSON.parse(localStorage.getItem("cartItems"));
     // console.log(varijabla);
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist) {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
-        )
-        // console.log("test")
-        // localStorage.setItem("userInfo", JSON.stringify(data))
-      );
-    } else {
-      setCartItems([...cartItems, { ...product, qty: 1 }]);
-    }
+    // const exist = cartItems.find((x) => x.id === product.id);
+    // if (exist) {
+    //   setCartItems(
+    //     cartItems.map((x) =>
+    //       x.id === product.id ? { ...exist, qty: exist.qty + 1 } : x
+    //     )
+    //     // console.log("test")
+    //     // localStorage.setItem("userInfo", JSON.stringify(data))
+    //   );
+    // } else {
+    //   setCartItems([...cartItems, { ...product, qty: 1 }]);
+    // }
   };
   const onRemove = (product) => {
-    const exist = cartItems.find((x) => x.id === product.id);
-    if (exist.qty === 1) {
-      setCartItems(cartItems.filter((x) => x.id !== product.id));
-    } else {
-      setCartItems(
-        cartItems.map((x) =>
-          x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
-        )
-      );
-    }
+    // const exist = cartItems.find((x) => x.id === product.id);
+    // if (exist.qty === 1) {
+    //   setCartItems(cartItems.filter((x) => x.id !== product.id));
+    // } else {
+    //   setCartItems(
+    //     cartItems.map((x) =>
+    //       x.id === product.id ? { ...exist, qty: exist.qty - 1 } : x
+    //     )
+    //   );
+    // }
   };
 
   // const quantityDiscount = (product) => {
